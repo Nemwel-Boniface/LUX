e5b9d60e56dcc99ac001bc69ecbb5d377c4ac5fd
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-21 09:35:47 +0300

    Week 1 sol

---
+++ week1/README.md
@@ -0,0 +1,23 @@
+## Simpla array sum
+
+> Given an array of integers, find the sum of its elements.
+
+Constraints
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+`6 `
+`1 2 3 4 10 11`
+
+Sample Output
+
+`31`
+
+Explanation
+
+We print the sum of the array's elements:
+1+2+3+4+10+11 = 21
---
+++ week1/simpleArraySum.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+function simpleArraySum(ar) {
+    // Write your code here
+    let sum = 0;
+    ar.forEach(elem => {
+        sum += elem;
+    })
+    return sum;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = simpleArraySum(ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
